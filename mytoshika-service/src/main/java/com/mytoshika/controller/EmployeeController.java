50d526caa7fce657b3977550d92416e4939aea1c
mytoshikaitsolutions <53996653+mytoshikaitsolutions@users.noreply.github.com>
2021-08-19 18:14:43 +0530

    Revert "Added test cases"

--- mytoshika-service/src/main/java/com/mytoshika/controller/EmployeeController.java
+++ mytoshika-service/src/main/java/com/mytoshika/controller/EmployeeController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,11 +18,8 @@ import com.mytoshika.dto.Status;
 import com.mytoshika.service.EmployeeService;
 
 import io.micrometer.core.annotation.Timed;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 
-@Api(tags="Employee controller provider" , description="This is a employee contoller to provide all the employee related APIs")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/employee")
@@ -31,7 +27,6 @@ public class EmployeeController {
 
 	private EmployeeService employeeService;
 	
-	@ApiOperation(value = "API to add employee details")
 	@Timed
 	@PostMapping("/add")
 	private ResponseEntity<ResponseBodyDTO<EmployeeDto>> addEmployee(@RequestBody EmployeeDto employeeDto) throws Exception{
@@ -42,7 +37,6 @@ public class EmployeeController {
 		return new ResponseEntity<>(responseBodyDTO, HttpStatus.OK);
 	}
 	
-	@ApiOperation(value = "API to update employee details by Id")
 	@PutMapping("/update")
 	private ResponseEntity<ResponseBodyDTO<EmployeeDto>> updateEmployeeDetails(@RequestBody EmployeeDto employeeDto){
 		ResponseBodyDTO<EmployeeDto> responseBodyDTO = new ResponseBodyDTO<>();
@@ -52,7 +46,6 @@ public class EmployeeController {
 		return new ResponseEntity<>(responseBodyDTO, HttpStatus.OK);
 	}
 	
-	@ApiOperation(value = "API to get employee details by Id")
 	@GetMapping("/{id}")
 	private ResponseEntity<ResponseBodyDTO<EmployeeDto>> getEmployeeById(@PathVariable("id") String id){
 		ResponseBodyDTO<EmployeeDto> responseBodyDTO = new ResponseBodyDTO<>();
@@ -62,7 +55,6 @@ public class EmployeeController {
 		return new ResponseEntity<>(responseBodyDTO, HttpStatus.OK);
 	}
 	
-	@ApiOperation(value = "API to get all employee details")
 	@GetMapping()
 	private ResponseEntity<ResponseBodyDTO<List<EmployeeDto>>> getEmployeeList(){
 		ResponseBodyDTO<List<EmployeeDto>> responseBodyDTO = new ResponseBodyDTO<>();
@@ -70,11 +62,5 @@ public class EmployeeController {
 		responseBodyDTO.setMessage("Employee details");
 		responseBodyDTO.setStatus(Status.SUCCESS);
 		return new ResponseEntity<>(responseBodyDTO, HttpStatus.OK);
-	}
-	
-	@ApiOperation(value = "API to delete employee details by Id")
-	@DeleteMapping("/delete/{id}")
-	public ResponseEntity<Boolean> deleteEmployeeDetails(@PathVariable("id") String id) {
-		return ResponseEntity.ok(employeeService.deleteEmployeeDetails(id));
 	}
 }
--- mytoshika-service/src/test/java/com/mytoshika/dao/EmployeeDtoTest.java
+++
@@ -1,5 +0,0 @@
-package com.mytoshika.dao;
-
-public class EmployeeDtoTest {
-
-}
