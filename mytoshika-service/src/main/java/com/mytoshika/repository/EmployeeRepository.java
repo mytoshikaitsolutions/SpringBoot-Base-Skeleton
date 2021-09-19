c75ecac6a01d1e686c0bce0903677080f830fd00
saurabh <mytoshika@gmail.com>
2021-09-19 21:32:19 +0530

    newly updated code

--- mytoshika-service/src/main/java/com/mytoshika/repository/EmployeeRepository.java
+++ mytoshika-service/src/main/java/com/mytoshika/repository/EmployeeRepository.java
@@ -1,11 +1,17 @@
 package com.mytoshika.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.mytoshika.domain.EmployeeEntity;
+import com.mytoshika.entity.EmployeeEntity;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, String>  {
+
+	Optional<EmployeeEntity> findById(String id);
+
+	void deleteById(String id);
 
 }
---
+++ mytoshika-service/src/test/java/com/mytoshika/serviceImpl/EmployeeServiceImplTest.java
@@ -0,0 +1,5 @@
+package com.mytoshika.serviceImpl;
+
+public class EmployeeServiceImplTest {
+
+}
